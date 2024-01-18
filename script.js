e24359b18716edb85bd21351f6cdcfd0510be49c
Finn Moffett <finn.moffett@gmail.com>
2024-01-18 19:13:40 +0000

    Format grid and add hover effect
    Format grid so that the overall size can be set.
    Set it so that the background color of each box changes when hovering
    with mouse.

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const container = document.querySelector('.container')
+const button = document.querySelector('button')
 
-for (let i = 0; i < 16; i++) {
+function gridSize() {
+  return parseInt(prompt('Please choose the number of rows and columns'))
+}
+
+button.addEventListener('click', () => {
+  gridSize()
+   
+})
+
+for (let i = 0; i < gridSize(); i++) {
   const row = document.createElement('div')
   container.appendChild(row)
   row.classList.add('row')
-  for (let j = 0; j < 16; j++) {
+  for (let j = 0; j < gridSize(); j++) {
     const box = document.createElement('div')
     row.appendChild(box)
     box.classList.add('box')
   }
 }
 
-/* const box = document.querySelector('.box') */
-box.addEventListener("mouseover", () => {box.classList.add('covered')});
+const boxes = document.querySelectorAll('.box'); 
+
+boxes.forEach((box) => box.addEventListener('mouseover', (event) => {
+  box.style.backgroundColor = 'red'}
+))
+
+
