541f851694a97ba5e9a173ddc15082fc8fc4d865
Finn Moffett <finn.moffett@gmail.com>
2024-01-19 13:58:21 +0000

    Add pschedelic toggle and grid toggle functions
    Add a button for toggling between black and colorful brushes. Add a
    button for toggling between grid and no grid.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const container = document.querySelector('.container');
 const button = document.querySelector('.button');
 const body = document.querySelector('body');
 const clearButton = document.querySelector('.clear');
+const gridButton = document.querySelector('.grid');
+const multiButton = document.querySelector('.multi');
 
 // Event listener that prompts user for a grid size upon clicking of the button. 
 // If the number is less or equal to 100 the grid creation function is run, otherwise a warning message is sent. 
@@ -33,23 +35,55 @@ function makeGrid(rowcol) {
 // If the user runs the mouse over a box that box will highlight in red. 
   const boxes = document.querySelectorAll('.box'); 
 
+  let setColor = false;
+
+  multiButton.addEventListener('click', () => {
+    if (setColor === false) {
+      setColor = true;
+    } else {
+      setColor = false;
+    }
+    darkFactor = 1;
+  });
+
   function colorGen() {
     return Math.floor(Math.random() * 255) + 1
   };
   
   let darkFactor = 1;
 
-  boxes.forEach((box) => box.addEventListener('mouseover', () => {
-      darkFactor *= 0.995; 
-      box.style.backgroundColor = `rgb(${colorGen()*darkFactor},${colorGen()*darkFactor},${colorGen()*darkFactor})`;
-  }))
+  boxes.forEach((box) => box.addEventListener('mouseover', () => { 
+      if (setColor === true) {
+        darkFactor *= 0.995; 
+        box.style.backgroundColor = `rgb(${colorGen()*darkFactor},${colorGen()*darkFactor},${colorGen()*darkFactor})`;
+      } else {
+        darkFactor *= 0.995; 
+        box.style.backgroundColor = `black`;
+      }
+  }));
 
+  // Clear and return the color of brush to normal brightness. 
   function clearPage() {
-    boxes.forEach((box) => box.style.backgroundColor = '#ffffff')
+    boxes.forEach((box) => box.style.backgroundColor = '#ffffff');
     darkFactor = 1;
-  }
+  };
   
-  clearButton.addEventListener('click', () => {clearPage()})
+  clearButton.addEventListener('click', () => {clearPage()});
+
+  let gridOn = false;
+
+  function gridToggle() {
+    if (gridOn === false) {
+      boxes.forEach((box) => box.classList.add('grid'))
+      gridOn = true;
+    } else {
+      boxes.forEach((box) => box.classList.remove('grid'))
+      gridOn = false;
+    }
+  };
+
+  gridButton.addEventListener('click', () => {gridToggle()});
+
 };
 
 
